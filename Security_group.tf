e248cb9991e511c71a8dcabb5ad9ee86ddd266a9
Somya Bhardwaj <89398492+somya1925@users.noreply.github.com>
2024-02-11 13:01:22 +0530

    Create Security_group.tf

---
+++ Security_group.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1" # Update with your desired AWS region
+}
+ 
+resource "aws_security_group" "kubernetes_sg" {
+  name        = "kubernetes-sg"
+  description = "Security group for Kubernetes"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
